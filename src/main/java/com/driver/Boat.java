5914d65fb726d6c4da854a5ccd5dd099fa093b14
Junaid Me <junaidjunni953@gmail.com>
2023-04-06 13:59:27 +0530

    another

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,29 @@
+package com.driver;
+
+class Boat implements WaterVehicle {
+    private String name;
+    private int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String getVehicleName() {
+        return name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+}
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,30 +4,3 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
-class Boat implements WaterVehicle {
-    private String name;
-    private int capacity;
-
-    public Boat(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-    }
-
-    @Override
-    public String getVehicleName() {
-        return name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    @Override
-    public int getVehicleCapacity() {
-        return capacity;
-    }
-}
